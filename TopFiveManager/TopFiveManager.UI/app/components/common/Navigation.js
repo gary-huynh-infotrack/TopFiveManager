e1cd5e9f7eca6578cf35966d25a9ab1a348f529c
Daniel <daniel.cai92@gmail.com>
2017-07-04 03:07:40 +1000

    removed example user

--- TopFiveManager/TopFiveManager.UI/app/components/common/Navigation.js
+++ TopFiveManager/TopFiveManager.UI/app/components/common/Navigation.js
@@ -18,15 +18,17 @@ class Navigation extends Component {
     }
 
     render() {
+        /*
+         <a data-toggle="dropdown" className="dropdown-toggle" href="#">
+                            <span className="clear"> <span className="block m-t-xs"> <strong className="font-bold">Example user</strong>
+                             </span> <span className="text-muted text-xs block">Example position<b className="caret"></b></span> </span> </a>*/
         return (
             <nav className="navbar-default navbar-static-side" role="navigation">
                     <ul className="nav metismenu" id="side-menu" ref="menu">
                         <li className="nav-header">
                             <div className="dropdown profile-element"> <span>
                              </span>
-                                <a data-toggle="dropdown" className="dropdown-toggle" href="#">
-                            <span className="clear"> <span className="block m-t-xs"> <strong className="font-bold">Example user</strong>
-                             </span> <span className="text-muted text-xs block">Example position<b className="caret"></b></span> </span> </a>
+                               
                                 <ul className="dropdown-menu animated fadeInRight m-t-xs">
                                     <li><a href="#"> Logout</a></li>
                                 </ul>
