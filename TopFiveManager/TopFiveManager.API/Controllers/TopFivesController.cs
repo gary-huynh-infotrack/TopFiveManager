f2eece9ac04dfd53da81261cd39188a9ebaeb68b
Gary Huynh <gary.huynh@infotrack.com.au>
2017-07-03 12:55:47 +1000

    renamed controller

--- TopFiveManager/TopFiveManager.API/Controllers/TopFivesController.cs
+++ TopFiveManager/TopFiveManager.API/Controllers/TopFivesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace TopFiveManager.API.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class TopFivesController : Controller
     {
         // GET api/values
         [HttpGet]
