b63a403f7c88214a0107610ea416a084715be945
Greg Grondecki <greg.grondecki@infotrack.com.au>
2017-07-03 18:09:15 +1000

    add employees controller

---
+++ TopFiveManager/TopFiveManager.DataAccess/Repo/EmployeeRepo.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Linq;
+using TopFiveManager.DataAccess.Models;
+
+namespace TopFiveManager.DataAccess.Repo
+{
+    public class EmployeeRepo : RepoBase
+    {
+        public IEnumerable<Employee> GetAll()
+        {
+            return Query(d => d.Query<Employee>("SELECT * FROM Employees"));
+        }
+
+        public Employee GetById(int id)
+        {
+            return Query(d => d.Query<Employee>("SELECT * FROM Employees WHERE Id = @Id", new { Id = id })).Single();
+        }
+    }
+}
