6d988c0f0614fdee2c915e213bede9c803fcb85c
Greg Grondecki <greg.grondecki@infotrack.com.au>
2017-07-03 13:47:49 +1000

    add dapper and getting my top fives

---
+++ TopFiveManager/TopFiveManager.DataAccess/Repo/DepartmentRepo.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using TopFiveManager.DataAccess.Models;
+
+namespace TopFiveManager.DataAccess.Repo
+{
+    public class DepartmentRepo : RepoBase
+    {
+        public IEnumerable<Department> GetAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<Department>("SELECT * FROM Departments");
+            }
+        }
+    }
+}
---
+++ TopFiveManager/TopFiveManager.DataAccess/Repo/TopFivesRepo.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using TopFiveManager.DataAccess.Models;
+
+namespace TopFiveManager.DataAccess.Repo
+{
+    public class TopFivesRepo : RepoBase
+    {
+        public IEnumerable<TopFive> GetByUserId(int employeeId)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+
+                // looks up only by employee id, should also take into account the third
+                return dbConnection.Query<TopFive>(
+                    "GetTopFivesByEmployeeId", 
+                    new { EmployeeId = employeeId }, 
+                    null, 
+                    true, 
+                    null,
+                    CommandType.StoredProcedure);
+            }
+        }
+    }
+}
